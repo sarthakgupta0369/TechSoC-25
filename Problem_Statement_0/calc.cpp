99092be699df135d853e1b3fdaca41e25723e7f2
sarthakgupta0369 <sarthakgupta0369@gmail.com>
2025-08-10 01:13:27 +0530

    multi ops calc, no bodmas

--- Problem_Statement_0/calc.cpp
+++ Problem_Statement_0/calc.cpp
@@ -1,34 +1,50 @@
+
 #include <iostream>
+#include<vector>
+#include<string>
+
 using namespace std;
 
-
-
-
 int main() {
-    double a, b;
-    char op;
-
-
     while (true) {
-        cout << ">>>";
-        cin >> a >> op >> b;
-
-        if (op == '+') {
-            cout << a + b<<endl;
-        }
-        else if (op == '-') {
-            cout << a - b<<endl;
-        }
-        else if (op == '*') {
-            cout << a * b<<endl;
-        }
-        else if (op == '/') {
-            if (b == 0) {
-                cout << "undefined \n";
-                continue;
+        string ex;
+        cout << "-->";
+        getline(cin, ex);
+        vector<long double> numbers;
+        vector<char> ops;
+        string numstr;
+        
+        for (char c : ex) {
+            if (c != '+' && c != '-' && c != '=' && c != '*' && c != '/') {
+                numstr = numstr + c;
             }
-            cout << a / b<<endl;
+            else if (c == '+' || c == '-' || c == '=' || c == '*' || c == '/') {
+                numbers.push_back(stoll(numstr));
+                ops.push_back(c);
+                numstr = "";
+            }
+
         }
+        ops.pop_back();
+
+        long double ans = numbers[0];
+
+        for (int i = 0; i < ops.size(); i++) {
+            if (ops[i] == '+') {
+                ans = ans + numbers[i + 1];
+            }
+            else if (ops[i] == '-') {
+                ans = ans - numbers[i + 1];
+            }
+            else if (ops[i] == '*') {
+                ans = ans * numbers[i + 1];
+            }
+            else if (ops[i] == '/') {
+                ans = ans / numbers[i + 1];
+            }
+        }
+        cout << ans<<endl;
+
     }
     return 0;
 }
